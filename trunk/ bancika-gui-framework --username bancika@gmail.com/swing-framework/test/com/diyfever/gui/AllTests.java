c7a4758c8db408e3614eabfe26576e81b2fd3c3d
bancika <bancika@529568d8-ed95-11de-bf31-4dec8810c1c1>
2011-08-29 21:32:53 +0000

    move to new project directory names

---
+++ trunk/ bancika-gui-framework --username bancika@gmail.com/swing-framework/test/com/diyfever/gui/AllTests.java
@@ -0,0 +1,13 @@
+package com.diyfever.gui;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.diyfever.gui.objecttable.ObjectListTableModelTest;
+
+@RunWith(Suite.class)
+@SuiteClasses( {  ObjectListTableModelTest.class })
+public class AllTests {
+
+}
