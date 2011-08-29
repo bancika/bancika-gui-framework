3d621fffcafcf3b46b18b3f193e462e1ccc777d3
bancika <bancika@529568d8-ed95-11de-bf31-4dec8810c1c1>
2011-08-29 21:28:01 +0000

    move to new project directory names

---
+++ trunk/ bancika-gui-framework --username bancika@gmail.com/app-framework/test/com/diyfever/gui/miscutils/PropertyInjectorTest.java
@@ -0,0 +1,64 @@
+package com.diyfever.gui.miscutils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.awt.Color;
+import java.util.Properties;
+
+import org.junit.Test;
+
+public class PropertyInjectorTest {
+
+	public static int intValue;
+	public static double doubleValue;
+	public static String stringValue;
+	public static Color colorValue;
+	public static Boolean booleanValue;
+
+	@Test
+	public void testInjectPropretiesInt() {
+		Properties props = new Properties();
+		int expected = 15;
+		props.put(PropertyInjectorTest.class.getName() + ".intValue", expected);
+		PropertyInjector.injectProperties(props);
+		assertEquals(expected, intValue);
+	}
+
+	@Test
+	public void testInjectPropretiesDouble() {
+		Properties props = new Properties();
+		double expected = 123.456;
+		props.put(PropertyInjectorTest.class.getName() + ".doubleValue", Double.toString(expected));
+		PropertyInjector.injectProperties(props);
+		assertEquals(expected, doubleValue, 1e-10);
+	}
+
+	@Test
+	public void testInjectPropretiesBoolean() {
+		Properties props = new Properties();
+		boolean expected = true;
+		props.put(PropertyInjectorTest.class.getName() + ".booleanValue", Boolean
+				.toString(expected));
+		PropertyInjector.injectProperties(props);
+		assertEquals(expected, booleanValue);
+	}
+
+	@Test
+	public void testInjectPropretiesString() {
+		Properties props = new Properties();
+		boolean expected = true;
+		props.put(PropertyInjectorTest.class.getName() + ".stringValue", expected);
+		PropertyInjector.injectProperties(props);
+		assertEquals(expected, stringValue);
+	}
+
+	@Test
+	public void testInjectPropretiesColor() {
+		Properties props = new Properties();
+		String expectedHex = "#FFCC00";
+		Color expected = Color.decode(expectedHex);
+		props.put(PropertyInjectorTest.class.getName() + ".colorValue", expectedHex);
+		PropertyInjector.injectProperties(props);
+		assertEquals(expected, colorValue);
+	}
+}
