1798a5d54dd10b56ebaccbe34930570f49b801b3
bancika <bancika@529568d8-ed95-11de-bf31-4dec8810c1c1>
2012-08-17 14:26:17 +0000

    UNICODE font export to PDF files

---  bancika-gui-framework --username bancika@gmail.com/swing-framework/src/org/diylc/swingframework/export/DrawingExporter.java
+++  bancika-gui-framework --username bancika@gmail.com/swing-framework/src/org/diylc/swingframework/export/DrawingExporter.java
@@ -15,6 +15,8 @@ import java.awt.print.PrinterJob;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -23,6 +25,7 @@ import org.diylc.swingframework.IDrawingProvider;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.DefaultFontMapper;
 import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfTemplate;
@@ -44,7 +47,8 @@ public class DrawingExporter {
 	private static final double margin = 0; // 1cm
 	private static final int PDF_RESOLUTION = 72;
 	private static final int PNG_RESOLUTION = 300;
-	private static final int SCREEN_RESOLUTION = Toolkit.getDefaultToolkit().getScreenResolution();
+	private static final int SCREEN_RESOLUTION = Toolkit.getDefaultToolkit()
+			.getScreenResolution();
 
 	private static DrawingExporter instance;
 
@@ -64,13 +68,14 @@ public class DrawingExporter {
 	 * @param plugInPort
 	 * @throws PrinterException
 	 */
-	public void print(final IDrawingProvider plugInPort) throws PrinterException {
+	public void print(final IDrawingProvider plugInPort)
+			throws PrinterException {
 		PrinterJob printJob = PrinterJob.getPrinterJob();
 		printJob.setPrintable(new Printable() {
 
 			@Override
-			public int print(Graphics graphics, PageFormat pageFormat, int pageIndex)
-					throws PrinterException {
+			public int print(Graphics graphics, PageFormat pageFormat,
+					int pageIndex) throws PrinterException {
 				if (pageIndex > 0) {
 					return (NO_SUCH_PAGE);
 				} else {
@@ -78,20 +83,23 @@ public class DrawingExporter {
 
 					Dimension d = plugInPort.getSize();
 
-					double pageRatio = pageFormat.getWidth() / pageFormat.getHeight();
+					double pageRatio = pageFormat.getWidth()
+							/ pageFormat.getHeight();
 					double imageRatio = d.getWidth() / d.getHeight();
 					double scale;
 					if (imageRatio > pageRatio) {
-						scale = (pageFormat.getWidth() - 2 * margin) / d.getWidth();
+						scale = (pageFormat.getWidth() - 2 * margin)
+								/ d.getWidth();
 					} else {
-						scale = (pageFormat.getHeight() - 2 * margin) / d.getHeight();
+						scale = (pageFormat.getHeight() - 2 * margin)
+								/ d.getHeight();
 					}
 					if (scale > 1) {
 						scale = 1d;
 					}
 
-					g2d.translate(pageFormat.getImageableX() + margin, pageFormat.getImageableY()
-							+ margin);
+					g2d.translate(pageFormat.getImageableX() + margin,
+							pageFormat.getImageableY() + margin);
 
 					g2d.setFont(new Font("Tahoma", Font.PLAIN, 6));
 					FontMetrics metrics = g2d.getFontMetrics();
@@ -153,8 +161,8 @@ public class DrawingExporter {
 
 					if (scale < 1) {
 						String warningStr = "Note: image has been scaled down to fit the page.";
-						g2d.drawString(warningStr, 0, (int) (d.getHeight() * scale + metrics
-								.getHeight()));
+						g2d.drawString(warningStr, 0, (int) (d.getHeight()
+								* scale + metrics.getHeight()));
 					}
 
 					g2d.scale(scale, scale);
@@ -178,20 +186,31 @@ public class DrawingExporter {
 	 * @throws DocumentException
 	 * @throws FileNotFoundException
 	 */
-	public void exportPDF(IDrawingProvider plugInPort, File file) throws FileNotFoundException,
-			DocumentException {
+	public void exportPDF(IDrawingProvider plugInPort, File file)
+			throws FileNotFoundException, DocumentException {
 		Dimension d = plugInPort.getSize();
 		// We have to scale everything down because PDF resolution is slightly
 		// lower.
 		float factor = 1f * PDF_RESOLUTION / SCREEN_RESOLUTION;
 		float totalWidth = (float) (factor * (2 * margin + d.getWidth()));
 		float totalHeight = (float) (factor * (2 * margin + d.getHeight()));
-		Document document = new Document(new com.lowagie.text.Rectangle(totalWidth, totalHeight));
-		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
+		Document document = new Document(new com.lowagie.text.Rectangle(
+				totalWidth, totalHeight));
+		PdfWriter writer = PdfWriter.getInstance(document,
+				new FileOutputStream(file));
 		document.open();
 		PdfContentByte contentByte = writer.getDirectContent();
-		PdfTemplate template = contentByte.createTemplate(totalWidth, totalHeight);
-		DefaultFontMapper mapper = new DefaultFontMapper();
+		PdfTemplate template = contentByte.createTemplate(totalWidth,
+				totalHeight);
+		DefaultFontMapper mapper = new DefaultFontMapper() {
+			@Override
+			public BaseFontParameters getBaseFontParameters(String arg0) {
+				BaseFontParameters p = super.getBaseFontParameters(arg0);
+				if (p != null)
+					p.encoding = BaseFont.IDENTITY_H;
+				return p;
+			}
+		};
 		if (Utils.isWindows()) {
 			mapper.insertDirectory(System.getenv("windir") + "\\Fonts");
 		} else if (Utils.isMac()) {
@@ -199,8 +218,15 @@ public class DrawingExporter {
 		} else if (Utils.isUnix()) {
 			mapper.insertDirectory("/usr/share/fonts/truetype/");
 		}
-		Graphics2D g2d = template.createGraphics((float) (factor * d.getWidth()),
-				(float) (factor * d.getHeight()), mapper);
+		// Map map = mapper.getMapper();
+		// for (Iterator i = map.keySet().iterator(); i.hasNext();) {
+		// String name = (String) i.next();
+		// System.out.println(name + ": "
+		// + ((DefaultFontMapper.BaseFontParameters) map.get(name)).fontName);
+		// }
+		Graphics2D g2d = template.createGraphics(
+				(float) (factor * d.getWidth()), (float) (factor * d
+						.getHeight()), mapper);
 		g2d.scale(factor, factor);
 
 		plugInPort.draw(g2d);
@@ -220,8 +246,9 @@ public class DrawingExporter {
 		try {
 			Dimension d = plugInPort.getSize();
 			double factor = 1f * PNG_RESOLUTION / SCREEN_RESOLUTION;
-			BufferedImage image = new BufferedImage((int) (d.getWidth() * factor), (int) (d
-					.getHeight() * factor), BufferedImage.TYPE_INT_RGB);
+			BufferedImage image = new BufferedImage(
+					(int) (d.getWidth() * factor),
+					(int) (d.getHeight() * factor), BufferedImage.TYPE_INT_RGB);
 			Graphics2D g2d = (Graphics2D) image.getGraphics();
 
 			g2d.scale(factor, factor);
