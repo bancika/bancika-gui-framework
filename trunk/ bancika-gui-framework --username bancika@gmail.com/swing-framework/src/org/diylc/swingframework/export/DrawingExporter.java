cd0b6d215eb9323992c5c66cd3ba5de4457bb57a
bancika <bancika@529568d8-ed95-11de-bf31-4dec8810c1c1>
2012-08-24 11:10:29 +0000

    multi-page support

--- trunk/ bancika-gui-framework --username bancika@gmail.com/swing-framework/src/org/diylc/swingframework/export/DrawingExporter.java
+++ trunk/ bancika-gui-framework --username bancika@gmail.com/swing-framework/src/org/diylc/swingframework/export/DrawingExporter.java
@@ -15,8 +15,6 @@ import java.awt.print.PrinterJob;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.Iterator;
-import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -65,23 +63,23 @@ public class DrawingExporter {
 	/**
 	 * Prints the image and scales it down if needed.
 	 * 
-	 * @param plugInPort
+	 * @param provider
 	 * @throws PrinterException
 	 */
-	public void print(final IDrawingProvider plugInPort)
-			throws PrinterException {
+	public void print(final IDrawingProvider provider) throws PrinterException {
 		PrinterJob printJob = PrinterJob.getPrinterJob();
+		final int pageCount = provider.getPageCount();
 		printJob.setPrintable(new Printable() {
 
 			@Override
 			public int print(Graphics graphics, PageFormat pageFormat,
 					int pageIndex) throws PrinterException {
-				if (pageIndex > 0) {
+				if (pageIndex >= pageCount) {
 					return (NO_SUCH_PAGE);
 				} else {
 					Graphics2D g2d = (Graphics2D) graphics;
 
-					Dimension d = plugInPort.getSize();
+					Dimension d = provider.getSize();
 
 					double pageRatio = pageFormat.getWidth()
 							/ pageFormat.getHeight();
@@ -105,60 +103,6 @@ public class DrawingExporter {
 					FontMetrics metrics = g2d.getFontMetrics();
 					g2d.setColor(Color.gray);
 
-					//
-					// g2d.drawLine((int) (d.getWidth() * scale), (int)
-					// (d.getHeight() * scale) + 5,
-					// (int) (d.getWidth() * scale - 72 * scale),
-					// (int) (d.getHeight() * scale) + 5);
-					// g2d.drawLine((int) (d.getWidth() * scale - 72 * scale),
-					// (int) (d.getHeight() * scale) + 3,
-					// (int) (d.getWidth() * scale - 72 * scale),
-					// (int) (d.getHeight() * scale) + 7);
-					// g2d.drawLine((int) (d.getWidth() * scale - 72 / 2 *
-					// scale), (int) (d
-					// .getHeight() * scale) + 4,
-					// (int) (d.getWidth() * scale - 72 / 2 * scale),
-					// (int) (d.getHeight() * scale) + 6);
-					// g2d.drawLine((int) (d.getWidth() * scale), (int)
-					// (d.getHeight() * scale) + 3,
-					// (int) (d.getWidth() * scale), (int) (d.getHeight() *
-					// scale) + 7);
-					//
-					// g2d.setFont(new Font("Tahoma", Font.PLAIN, 6));
-					// String inStr = "in";
-					// g2d.drawString(inStr, (int) (d.getWidth() * scale - 72 *
-					// scale - 1 - metrics
-					// .stringWidth(inStr)), (int) (d.getHeight() * scale
-					// + metrics.getHeight() - 1));
-					//
-					// g2d.drawLine((int) (d.getWidth() * scale), (int)
-					// (d.getHeight() * scale) +
-					// 11,
-					// (int) (d.getWidth() * scale - 72 / 2.54 * scale),
-					// (int) (d.getHeight() * scale) + 11);
-					// g2d.drawLine((int) (d.getWidth() * scale - 72 / 2.54 / 2
-					// * scale), (int) (d
-					// .getHeight() * scale) + 10,
-					// (int) (d.getWidth() * scale - 72 / 2.54 / 2 * scale),
-					// (int) (d
-					// .getHeight() * scale) + 12);
-					// g2d.drawLine((int) (d.getWidth() * scale - 72 / 2.54 *
-					// scale), (int) (d
-					// .getHeight() * scale) + 9,
-					// (int) (d.getWidth() * scale - 72 / 2.54 * scale),
-					// (int) (d.getHeight() * scale) + 13);
-					// g2d.drawLine((int) (d.getWidth() * scale), (int)
-					// (d.getHeight() * scale) + 9,
-					// (int) (d.getWidth() * scale), (int) (d.getHeight() *
-					// scale) + 13);
-					//
-					// String cmStr = "cm";
-					// g2d.drawString(cmStr,
-					// (int) (d.getWidth() * scale - 72 / 2.54 * scale - 1 -
-					// metrics
-					// .stringWidth(cmStr)), (int) (d.getHeight() * scale
-					// + metrics.getHeight() + 5));
-
 					if (scale < 1) {
 						String warningStr = "Note: image has been scaled down to fit the page.";
 						g2d.drawString(warningStr, 0, (int) (d.getHeight()
@@ -167,7 +111,7 @@ public class DrawingExporter {
 
 					g2d.scale(scale, scale);
 
-					plugInPort.draw(g2d);
+					provider.draw(pageIndex, g2d);
 
 					return (PAGE_EXISTS);
 				}
@@ -181,14 +125,14 @@ public class DrawingExporter {
 	/**
 	 * Creates a PDF in the same size as the project.
 	 * 
-	 * @param plugInPort
+	 * @param provider
 	 * @param file
 	 * @throws DocumentException
 	 * @throws FileNotFoundException
 	 */
-	public void exportPDF(IDrawingProvider plugInPort, File file)
+	public void exportPDF(IDrawingProvider provider, File file)
 			throws FileNotFoundException, DocumentException {
-		Dimension d = plugInPort.getSize();
+		Dimension d = provider.getSize();
 		// We have to scale everything down because PDF resolution is slightly
 		// lower.
 		float factor = 1f * PDF_RESOLUTION / SCREEN_RESOLUTION;
@@ -199,9 +143,6 @@ public class DrawingExporter {
 		PdfWriter writer = PdfWriter.getInstance(document,
 				new FileOutputStream(file));
 		document.open();
-		PdfContentByte contentByte = writer.getDirectContent();
-		PdfTemplate template = contentByte.createTemplate(totalWidth,
-				totalHeight);
 		DefaultFontMapper mapper = new DefaultFontMapper() {
 			@Override
 			public BaseFontParameters getBaseFontParameters(String arg0) {
@@ -224,39 +165,66 @@ public class DrawingExporter {
 		// System.out.println(name + ": "
 		// + ((DefaultFontMapper.BaseFontParameters) map.get(name)).fontName);
 		// }
-		Graphics2D g2d = template.createGraphics(
-				(float) (factor * d.getWidth()), (float) (factor * d
-						.getHeight()), mapper);
-		g2d.scale(factor, factor);
 
-		plugInPort.draw(g2d);
-
-		g2d.dispose();
-		contentByte.addTemplate(template, (float) margin, (float) margin);
+		for (int i = 0; i < provider.getPageCount(); i++) {
+			if (i > 0) {
+				document.newPage();
+			}
+			PdfContentByte contentByte = writer.getDirectContent();
+			PdfTemplate template = contentByte.createTemplate(totalWidth,
+					totalHeight);
+			Graphics2D g2d = template.createGraphics((float) (factor * d
+					.getWidth()), (float) (factor * d.getHeight()), mapper);
+			g2d.scale(factor, factor);
+			provider.draw(i, g2d);
+			contentByte.addTemplate(template, (float) margin, (float) margin);
+			g2d.dispose();
+		}
 		document.close();
 	}
 
 	/**
 	 * Renders the project into a PNG file.
 	 * 
-	 * @param plugInPort
+	 * @param provider
 	 * @param file
 	 */
-	public void exportPNG(IDrawingProvider plugInPort, File file) {
+	public void exportPNG(IDrawingProvider provider, File file) {
 		try {
-			Dimension d = plugInPort.getSize();
+			int pageCount = provider.getPageCount();
+			Dimension d = provider.getSize();
 			double factor = 1f * PNG_RESOLUTION / SCREEN_RESOLUTION;
-			BufferedImage image = new BufferedImage(
-					(int) (d.getWidth() * factor),
-					(int) (d.getHeight() * factor), BufferedImage.TYPE_INT_RGB);
-			Graphics2D g2d = (Graphics2D) image.getGraphics();
 
-			g2d.scale(factor, factor);
-			plugInPort.draw(g2d);
+			if (pageCount == 1) {
+				BufferedImage image = new BufferedImage(
+						(int) (d.getWidth() * factor),
+						(int) (d.getHeight() * factor),
+						BufferedImage.TYPE_INT_RGB);
+				Graphics2D g2d = (Graphics2D) image.getGraphics();
 
-			g2d.dispose();
+				g2d.scale(factor, factor);
+				provider.draw(0, g2d);
+				g2d.dispose();
 
-			ImageIO.write(image, "PNG", file);
+				ImageIO.write(image, "PNG", file);
+			} else {
+				for (int i = 0; i < pageCount; i++) {
+					BufferedImage image = new BufferedImage(
+							(int) (d.getWidth() * factor),
+							(int) (d.getHeight() * factor),
+							BufferedImage.TYPE_INT_RGB);
+					Graphics2D g2d = (Graphics2D) image.getGraphics();
+
+					g2d.scale(factor, factor);
+					// Draw a page
+					provider.draw(i, g2d);
+					// Move down
+					g2d.translate(0, (int) (d.getHeight() * factor));
+					g2d.dispose();
+					ImageIO.write(image, "PNG", new File(file.getAbsolutePath()
+							.replaceAll("\\.png", "_" + (i + 1) + ".png")));
+				}
+			}
 		} catch (Exception e) {
 			System.out.println("Error exporting: " + e);
 		}
