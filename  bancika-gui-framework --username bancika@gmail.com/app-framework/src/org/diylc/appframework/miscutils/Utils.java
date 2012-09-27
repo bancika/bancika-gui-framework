8034995f75868a85d7290c22e3c8c26a39293e05
bancika <bancika@529568d8-ed95-11de-bf31-4dec8810c1c1>
2012-09-27 16:17:45 +0000

    classpath util method

---  bancika-gui-framework --username bancika@gmail.com/app-framework/src/org/diylc/appframework/miscutils/Utils.java
+++  bancika-gui-framework --username bancika@gmail.com/app-framework/src/org/diylc/appframework/miscutils/Utils.java
@@ -2,15 +2,25 @@ package org.diylc.appframework.miscutils;
 
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
+import org.apache.log4j.Logger;
 
 public class Utils {
+
+	private static final Logger LOG = Logger.getLogger(Utils.class);
 
 	static final String[] browsers = { "google-chrome", "firefox", "opera",
 			"epiphany", "konqueror", "conkeror", "midori", "kazehakase",
@@ -101,25 +111,82 @@ public class Utils {
 		return (os.indexOf("sunos") >= 0);
 	}
 
-	public static String[] getClasspath(Class<?> clazz) {
-		try {
-			URL url = clazz.getResource(clazz.getSimpleName() + ".class");
-			String name = url.toString();
-			name = name.substring(0, name.length()
-					- clazz.getName().replace('.', '/').concat(".class")
-							.length());
-			name += "META-INF/MANIFEST.MF";
-			url = new URL(name);
-			InputStream is = url.openStream();
-			Manifest manifest = new Manifest();
-			manifest.read(is);
-			Attributes attributes = manifest.getMainAttributes();
-			Object value = attributes.get(Attributes.Name.CLASS_PATH);
-			return value.toString().split(" ");
-		} catch (Exception e) {
-			return System.getProperty("java.class.path").split(
-					isWindows() ? ";" : ":");
+	public static Set<Class<?>> getClasses(String packageName) throws Exception {
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		return getClasses(loader, packageName);
+	}
+
+	public static Set<Class<?>> getClasses(ClassLoader loader, String packageName)
+			throws IOException, ClassNotFoundException {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		String path = packageName.replace('.', '/');
+		Enumeration<URL> resources = loader.getResources(path);
+		if (resources != null) {
+			while (resources.hasMoreElements()) {
+				String filePath = resources.nextElement().getFile();
+				// WINDOWS HACK
+				if (filePath.indexOf("%20") > 0)
+					filePath = filePath.replaceAll("%20", " ");
+				if (filePath != null) {
+					if ((filePath.indexOf("!") > 0)
+							& (filePath.indexOf(".jar") > 0)) {
+						String jarPath = filePath.substring(0,
+								filePath.indexOf("!")).substring(
+								filePath.indexOf(":") + 1);
+						// WINDOWS HACK
+						if (jarPath.indexOf(":") >= 0)
+							jarPath = jarPath.substring(1);
+						classes.addAll(getFromJARFile(jarPath, path));
+					} else {
+						classes.addAll(getFromDirectory(new File(filePath),
+								packageName));
+					}
+				}
+			}
 		}
+		return classes;
+	}
+
+	public static Set<Class<?>> getFromDirectory(File directory, String packageName)
+			throws ClassNotFoundException {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		if (directory.exists()) {
+			for (String file : directory.list()) {
+				if (file.endsWith(".class")) {
+					String name = packageName + '.'
+							+ stripFilenameExtension(file);
+					Class<?> clazz = Class.forName(name);
+					classes.add(clazz);
+				}
+			}
+		}
+		return classes;
+	}
+
+	private static String stripFilenameExtension(String file) {
+		int i = file.length() - 1;
+		while (file.charAt(i) != '.' && i > 0)
+			i--;
+		return file.substring(0, i);
+	}
+
+	public static Set<Class<?>> getFromJARFile(String jar, String packageName)
+			throws FileNotFoundException, IOException, ClassNotFoundException {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		JarInputStream jarFile = new JarInputStream(new FileInputStream(jar));
+		JarEntry jarEntry;
+		do {
+			jarEntry = jarFile.getNextJarEntry();
+			if (jarEntry != null) {
+				String className = jarEntry.getName();
+				if (className.endsWith(".class")) {
+					className = stripFilenameExtension(className);
+					if (className.startsWith(packageName))
+						classes.add(Class.forName(className.replace('/', '.')));
+				}
+			}
+		} while (jarEntry != null);
+		return classes;
 	}
 
 	/**
