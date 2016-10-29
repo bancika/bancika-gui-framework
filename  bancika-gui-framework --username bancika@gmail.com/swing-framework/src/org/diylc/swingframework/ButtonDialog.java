5e0e028ad463ab9fe10b6a6db3b844b2ea6291ba
Branislav Stojkovic <Branislav Stojkovic@Bancika-PC>
2016-10-29 12:42:39 +0200

    set default button of a dialog, close on escape

---  bancika-gui-framework --username bancika@gmail.com/swing-framework/src/org/diylc/swingframework/ButtonDialog.java
+++  bancika-gui-framework --username bancika@gmail.com/swing-framework/src/org/diylc/swingframework/ButtonDialog.java
@@ -3,91 +3,117 @@ package org.diylc.swingframework;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.KeyStroke;
 
 public abstract class ButtonDialog extends JDialog {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	public static final String OK = "OK";
-	public static final String CANCEL = "Cancel";
+  public static final String OK = "OK";
+  public static final String CANCEL = "Cancel";
 
-	private JPanel containerPanel;
-	private JPanel buttonPanel;
+  private JPanel containerPanel;
+  private JPanel buttonPanel;
 
-	private String[] buttonCaptions;
+  private String[] buttonCaptions;
 
-	private Map<String, JButton> buttonMap;
+  private Map<String, JButton> buttonMap;
 
-	private String selectedButtonCaption;
+  private String selectedButtonCaption;
 
-	public ButtonDialog(JFrame owner, String title, String[] buttonCaptions) {
-		super(owner, title);
+  private static final KeyStroke escapeStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+  public static final String dispatchWindowClosingActionMapKey = "org.diylc.swingframework.dispatch:WINDOW_CLOSING";
 
-		setModal(true);
-		setResizable(false);
+  public static void installEscapeCloseOperation(final JDialog dialog) {
+    Action dispatchClosing = new AbstractAction() {
 
-		this.buttonCaptions = buttonCaptions;
+      private static final long serialVersionUID = 1L;
 
-		this.buttonMap = new HashMap<String, JButton>();
-	}
+      public void actionPerformed(ActionEvent event) {
+        dialog.dispatchEvent(new WindowEvent(dialog, WindowEvent.WINDOW_CLOSING));
+      }
+    };
+    JRootPane root = dialog.getRootPane();
+    root.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeStroke, dispatchWindowClosingActionMapKey);
+    root.getActionMap().put(dispatchWindowClosingActionMapKey, dispatchClosing);
+  }
 
-	protected void layoutGui() {
-		setContentPane(getContainerPanel());
-		pack();
-		setLocationRelativeTo(getParent());
-	}
+  public ButtonDialog(JFrame owner, String title, String[] buttonCaptions) {
+    super(owner, title);
 
-	public JButton getButton(String caption) {
-		return buttonMap.get(caption);
-	}
+    setModal(true);
+    setResizable(false);
 
-	public String getSelectedButtonCaption() {
-		return selectedButtonCaption;
-	}
+    this.buttonCaptions = buttonCaptions;
 
-	private JPanel getContainerPanel() {
-		if (containerPanel == null) {
-			containerPanel = new JPanel(new BorderLayout());
-			containerPanel.add(getMainComponent(), BorderLayout.CENTER);
-			containerPanel.add(getButtonPanel(), BorderLayout.SOUTH);
-		}
-		return containerPanel;
-	}
+    this.buttonMap = new HashMap<String, JButton>();
+  }
 
-	public JPanel getButtonPanel() {
-		if (buttonPanel == null) {
-			buttonPanel = new JPanel();
-			for (String caption : buttonCaptions) {
-				final String command = caption;
-				JButton button = new JButton(caption);
-				button.addActionListener(new ActionListener() {
+  protected void layoutGui() {
+    setContentPane(getContainerPanel());
+    pack();
+    setLocationRelativeTo(getParent());
+  }
 
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						if (validateInput(command)) {
-							selectedButtonCaption = command;
-							ButtonDialog.this.setVisible(false);
-						}
-					}
-				});
-				buttonPanel.add(button);
-				buttonMap.put(command, button);
-			}
-		}
-		return buttonPanel;
-	}
+  public JButton getButton(String caption) {
+    return buttonMap.get(caption);
+  }
 
-	abstract protected JComponent getMainComponent();
+  public String getSelectedButtonCaption() {
+    return selectedButtonCaption;
+  }
 
-	protected boolean validateInput(String button) {
-		return true;
-	}
+  private JPanel getContainerPanel() {
+    if (containerPanel == null) {
+      containerPanel = new JPanel(new BorderLayout());
+      containerPanel.add(getMainComponent(), BorderLayout.CENTER);
+      containerPanel.add(getButtonPanel(), BorderLayout.SOUTH);
+    }
+    return containerPanel;
+  }
+
+  public JPanel getButtonPanel() {
+    if (buttonPanel == null) {
+      buttonPanel = new JPanel();
+      for (String caption : buttonCaptions) {
+        final String command = caption;
+        JButton button = new JButton(caption);
+        button.addActionListener(new ActionListener() {
+
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            if (validateInput(command)) {
+              selectedButtonCaption = command;
+              ButtonDialog.this.setVisible(false);
+            }
+          }
+        });
+        buttonPanel.add(button);
+        buttonMap.put(command, button);
+      }
+      if (!buttonMap.isEmpty()) {
+        getRootPane().setDefaultButton(buttonMap.get(buttonCaptions[0]));
+      }
+    }
+    return buttonPanel;
+  }
+
+  abstract protected JComponent getMainComponent();
+
+  protected boolean validateInput(String button) {
+    return true;
+  }
 }
