1d0ad2c5bf730871a7e16eec041a0ac334a55475
Branislav Stojkovic <Branislav Stojkovic@Bancika-PC>
2016-10-30 09:11:37 +0100

    add DropDownButton

--- bancika-gui-framework/swing-framework/src/org/diylc/swingframework/images/IconLoader.java
+++ bancika-gui-framework/swing-framework/src/org/diylc/swingframework/images/IconLoader.java
@@ -10,22 +10,22 @@ import javax.swing.ImageIcon;
  */
 public enum IconLoader {
 
-	Error("error.png"), Warning("warning.png"), LightBulbOn("lightbulb_on.png"), LightBulbOff(
-			"lightbulb_off.png"), MoveSmall("move_small.png"), Undo("undo.png"), Redo("redo.png"), ;
+  Error("error.png"), Warning("warning.png"), LightBulbOn("lightbulb_on.png"), LightBulbOff("lightbulb_off.png"), MoveSmall(
+      "move_small.png"), Undo("undo.png"), Redo("redo.png"), Arrow("arrow.png");
 
-	protected String name;
+  protected String name;
 
-	private IconLoader(String name) {
-		this.name = name;
-	}
+  private IconLoader(String name) {
+    this.name = name;
+  }
 
-	public Icon getIcon() {
-		java.net.URL imgURL = getClass().getResource(name);
-		if (imgURL != null) {
-			return new ImageIcon(imgURL, name);
-		} else {
-			System.err.println("Couldn't find file: " + name);
-			return null;
-		}
-	}
+  public Icon getIcon() {
+    java.net.URL imgURL = getClass().getResource(name);
+    if (imgURL != null) {
+      return new ImageIcon(imgURL, name);
+    } else {
+      System.err.println("Couldn't find file: " + name);
+      return null;
+    }
+  }
 }
---
+++ bancika-gui-framework/swing-framework/src/org/diylc/swingframework/openide/DropDownButtonFactory.java
@@ -0,0 +1,13 @@
+package org.diylc.swingframework.openide;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JPopupMenu;
+
+public final class DropDownButtonFactory {
+  public static final String PROP_DROP_DOWN_MENU = "dropDownMenu";
+
+  public static JButton createDropDownButton(Icon icon, JPopupMenu dropDownMenu) {
+    return new DropDownButton(icon, dropDownMenu);
+  }
+}
