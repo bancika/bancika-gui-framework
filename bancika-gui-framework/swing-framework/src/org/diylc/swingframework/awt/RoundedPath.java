53ef4074205774c3d60026f0bacc028f3ac1c381
bancika <bancika@gmail.com>
2016-12-29 13:27:04 +0100

    javadoc added

--- bancika-gui-framework/swing-framework/src/org/diylc/swingframework/awt/RoundedPath.java
+++ bancika-gui-framework/swing-framework/src/org/diylc/swingframework/awt/RoundedPath.java
@@ -2,6 +2,22 @@ package org.diylc.swingframework.awt;
 
 import java.awt.geom.GeneralPath;
 
+/***
+ * 
+ * Creates a {@link GeneralPath} with rounded edges. To create a rounded path, follow these steps:
+ * 
+ * <ul>
+ * <li>instantiate a {@link RoundedPath}</li>
+ * <li>place a starting point using {@link RoundedPath#moveTo(double, double)}</li>
+ * <li>create a polygon using a series of {@link RoundedPath#lineTo(double, double)} calls</li>
+ * <li>call {@link RoundedPath#getPath()} to get a rounded path</li>
+ * </ul>
+ * 
+ * Note that the starting point shouldn't lie on a corner of the polygon if you want that corner to
+ * get rounded too. Instead, place the starting/ending point on one of the polygon sides.
+ * 
+ * @author bancika
+ */
 public class RoundedPath {
 
   private GeneralPath path;
@@ -30,18 +46,18 @@ public class RoundedPath {
     if (Double.isNaN(prevX)) {
       double theta = Math.atan2(y - this.y, x - this.x);
       double r = Math.sqrt((y - this.y) * (y - this.y) + (x - this.x) * (x - this.x));
-      path.lineTo(this.x + Math.cos(theta) * (r - radius), this.y + Math.sin(theta) * (r - radius));      
+      path.lineTo(this.x + Math.cos(theta) * (r - radius), this.y + Math.sin(theta) * (r - radius));
     } else {
-      double theta = Math.atan2(y - this.y, x - this.x);      
+      double theta = Math.atan2(y - this.y, x - this.x);
       path.curveTo(this.x, this.y, this.x, this.y, this.x + Math.cos(theta) * radius, this.y + Math.sin(theta) * radius);
     }
-    
+
     this.prevX = this.x;
     this.prevY = this.y;
     this.x = x;
     this.y = y;
   }
-  
+
   public GeneralPath getPath() {
     return path;
   }
