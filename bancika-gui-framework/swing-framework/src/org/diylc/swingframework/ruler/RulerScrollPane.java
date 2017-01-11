57e21bd84bc2ddb66090e06ce352a5dc4ff66a46
bancika <bancika@gmail.com>
2017-01-11 12:29:55 +0100

    show/hide rulers

--- bancika-gui-framework/swing-framework/src/org/diylc/swingframework/ruler/RulerScrollPane.java
+++ bancika-gui-framework/swing-framework/src/org/diylc/swingframework/ruler/RulerScrollPane.java
@@ -34,11 +34,10 @@ import org.diylc.swingframework.IDrawingProvider;
  * <li>button in the bottom-right corner to navigate through the viewport</li>
  * </ul>
  * 
- * Note: when doing drag&drop in the viewport component sometimes Java doesn't
- * fire <code>mouseDragged</code> events. To overcome this, component can notify
- * about drag movements by firing property change events. Property name is
- * "dragPoint" and x, y coordinates are stored as oldValue and newValue
- * respectively. <br>
+ * Note: when doing drag&drop in the viewport component sometimes Java doesn't fire
+ * <code>mouseDragged</code> events. To overcome this, component can notify about drag movements by
+ * firing property change events. Property name is "dragPoint" and x, y coordinates are stored as
+ * oldValue and newValue respectively. <br>
  * <br>
  * Note to self: try to come up with something more elegant.
  * 
@@ -46,190 +45,210 @@ import org.diylc.swingframework.IDrawingProvider;
  */
 public class RulerScrollPane extends JScrollPane {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	private Ruler horizontalRuler;
-	private Ruler verticalRuler;
-	private JButton unitButton;
-	private JButton navigateButton;
+  private Ruler horizontalRuler;
+  private Ruler verticalRuler;
+  private JButton unitButton;
+  private JButton navigateButton;
+  private Corner topRightCorner;
+  private Corner bottomLeftCorner;
 
-	private List<IRulerListener> listeners;
-	
-	public RulerScrollPane(Component view) {
-		this(view, new ComponentThumbnailProvider(view));
-	}
-	
-	public RulerScrollPane(Component view, final IDrawingProvider provider) {
-		this(view, provider, 0, 0);
-	}
+  private List<IRulerListener> listeners;
 
-	public RulerScrollPane(Component view, final IDrawingProvider provider, double cmSpacing, double inSpacing) {
-		super(view);
+  public RulerScrollPane(Component view) {
+    this(view, new ComponentThumbnailProvider(view));
+  }
 
-		horizontalRuler = new Ruler(Ruler.HORIZONTAL, true, cmSpacing, inSpacing);
-		setColumnHeaderView(horizontalRuler);
-		verticalRuler = new Ruler(Ruler.VERTICAL, true, cmSpacing, inSpacing);
-		setRowHeaderView(verticalRuler);
+  public RulerScrollPane(Component view, final IDrawingProvider provider) {
+    this(view, provider, 0, 0);
+  }
 
-		listeners = new ArrayList<IRulerListener>();
+  public RulerScrollPane(Component view, final IDrawingProvider provider, double cmSpacing, double inSpacing) {
+    super(view);
 
-		view.addComponentListener(new ComponentAdapter() {
+    horizontalRuler = new Ruler(Ruler.HORIZONTAL, true, cmSpacing, inSpacing);
+    verticalRuler = new Ruler(Ruler.VERTICAL, true, cmSpacing, inSpacing);
+    setColumnHeaderView(horizontalRuler);
+    setRowHeaderView(verticalRuler);
 
-			@Override
-			public void componentResized(ComponentEvent e) {
-				updateRulerSize(e.getComponent().getWidth(), e.getComponent().getHeight());
-			}
-		});
-		updateRulerSize(view.getWidth(), view.getHeight());
+    listeners = new ArrayList<IRulerListener>();
 
-		unitButton = new JButton("cm");
-		unitButton.setToolTipText("Toggle metric/imperial system");
-		unitButton.setMargin(new Insets(0, 0, 0, 0));
-		unitButton.setFont(unitButton.getFont().deriveFont(9f));
-		unitButton.setFocusable(false);
-		setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, unitButton);
-		unitButton.addActionListener(new ActionListener() {
+    view.addComponentListener(new ComponentAdapter() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (horizontalRuler.isMetric()) {
-					horizontalRuler.setIsMetric(false);
-					verticalRuler.setIsMetric(false);
-					unitButton.setText("in");
-				} else {
-					horizontalRuler.setIsMetric(true);
-					verticalRuler.setIsMetric(true);
-					unitButton.setText("cm");
-				}
-				for (IRulerListener listener : listeners) {
-					listener.unitsChanged(horizontalRuler.isMetric());
-				}
-			}
-		});
+      @Override
+      public void componentResized(ComponentEvent e) {
+        updateRulerSize(e.getComponent().getWidth(), e.getComponent().getHeight());
+      }
+    });
+    updateRulerSize(view.getWidth(), view.getHeight());
 
-		setCorner(ScrollPaneConstants.UPPER_RIGHT_CORNER, new Corner(Ruler.HORIZONTAL));
-		setCorner(ScrollPaneConstants.LOWER_LEFT_CORNER, new Corner(Ruler.VERTICAL));
+    unitButton = new JButton("cm");
+    unitButton.setToolTipText("Toggle metric/imperial system");
+    unitButton.setMargin(new Insets(0, 0, 0, 0));
+    unitButton.setFont(unitButton.getFont().deriveFont(9f));
+    unitButton.setFocusable(false);
+    unitButton.addActionListener(new ActionListener() {
 
-		navigateButton = new JButton(IconLoader.MoveSmall.getIcon());
-		navigateButton.setToolTipText("Auto-scroll");
-		navigateButton.setFocusable(false);
-		navigateButton.setMargin(new Insets(0, 0, 0, 0));
-		navigateButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (horizontalRuler.isMetric()) {
+          horizontalRuler.setIsMetric(false);
+          verticalRuler.setIsMetric(false);
+          unitButton.setText("in");
+        } else {
+          horizontalRuler.setIsMetric(true);
+          verticalRuler.setIsMetric(true);
+          unitButton.setText("cm");
+        }
+        for (IRulerListener listener : listeners) {
+          listener.unitsChanged(horizontalRuler.isMetric());
+        }
+      }
+    });
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				NavigateDialog navigateDialog = new NavigateDialog(RulerScrollPane.this, provider);
-				navigateDialog.setVisible(true);
-				navigateDialog.setLocationRelativeTo(navigateButton);
-			}
-		});
+    navigateButton = new JButton(IconLoader.MoveSmall.getIcon());
+    navigateButton.setToolTipText("Auto-scroll");
+    navigateButton.setFocusable(false);
+    navigateButton.setMargin(new Insets(0, 0, 0, 0));
+    navigateButton.addActionListener(new ActionListener() {
 
-		setCorner(ScrollPaneConstants.LOWER_RIGHT_CORNER, navigateButton);
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        NavigateDialog navigateDialog = new NavigateDialog(RulerScrollPane.this, provider);
+        navigateDialog.setVisible(true);
+        navigateDialog.setLocationRelativeTo(navigateButton);
+      }
+    });
 
-		view.addPropertyChangeListener(new PropertyChangeListener() {
+    topRightCorner = new Corner(Ruler.HORIZONTAL);
+    bottomLeftCorner = new Corner(Ruler.VERTICAL);
 
-			@Override
-			public void propertyChange(PropertyChangeEvent evt) {
-				if (evt.getPropertyName().equals("dragPoint")) {
-					horizontalRuler.setIndicatorValue(((Long) evt.getOldValue()).intValue());
-					horizontalRuler.repaint();
-					verticalRuler.setIndicatorValue(((Long) evt.getNewValue()).intValue());
-					verticalRuler.repaint();
-				}
-			}
-		});
+    setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, unitButton);
+    setCorner(ScrollPaneConstants.UPPER_RIGHT_CORNER, topRightCorner);
+    setCorner(ScrollPaneConstants.LOWER_LEFT_CORNER, bottomLeftCorner);
+    setCorner(ScrollPaneConstants.LOWER_RIGHT_CORNER, navigateButton);
 
-		view.addMouseMotionListener(new MouseAdapter() {
+    view.addPropertyChangeListener(new PropertyChangeListener() {
 
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				horizontalRuler.setIndicatorValue(e.getX());
-				horizontalRuler.repaint();
-				verticalRuler.setIndicatorValue(e.getY());
-				verticalRuler.repaint();
-			}
+      @Override
+      public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("dragPoint")) {
+          horizontalRuler.setIndicatorValue(((Long) evt.getOldValue()).intValue());
+          horizontalRuler.repaint();
+          verticalRuler.setIndicatorValue(((Long) evt.getNewValue()).intValue());
+          verticalRuler.repaint();
+        }
+      }
+    });
 
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				horizontalRuler.setIndicatorValue(e.getX());
-				horizontalRuler.repaint();
-				verticalRuler.setIndicatorValue(e.getY());
-				verticalRuler.repaint();
-			}
+    view.addMouseMotionListener(new MouseAdapter() {
 
-			@Override
-			public void mouseExited(MouseEvent e) {
-				horizontalRuler.setIndicatorValue(-1);
-				horizontalRuler.repaint();
-				verticalRuler.setIndicatorValue(-1);
-				verticalRuler.repaint();
-			}
-		});
+      @Override
+      public void mouseMoved(MouseEvent e) {
+        horizontalRuler.setIndicatorValue(e.getX());
+        horizontalRuler.repaint();
+        verticalRuler.setIndicatorValue(e.getY());
+        verticalRuler.repaint();
+      }
 
-		setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		getHorizontalScrollBar().setUnitIncrement(50);
-		setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		getVerticalScrollBar().setUnitIncrement(50);
-	}
-	
-	public void setSelectionRectangle(Rectangle2D rect) {
-		horizontalRuler.setSelectionRect(rect);
-		verticalRuler.setSelectionRect(rect);
-	}
+      @Override
+      public void mouseDragged(MouseEvent e) {
+        horizontalRuler.setIndicatorValue(e.getX());
+        horizontalRuler.repaint();
+        verticalRuler.setIndicatorValue(e.getY());
+        verticalRuler.repaint();
+      }
 
-	public boolean addUnitListener(IRulerListener e) {
-		return listeners.add(e);
-	}
+      @Override
+      public void mouseExited(MouseEvent e) {
+        horizontalRuler.setIndicatorValue(-1);
+        horizontalRuler.repaint();
+        verticalRuler.setIndicatorValue(-1);
+        verticalRuler.repaint();
+      }
+    });
 
-	public boolean removeUnitListener(IRulerListener o) {
-		return listeners.remove(o);
-	}
+    setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+    getHorizontalScrollBar().setUnitIncrement(50);
+    setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    getVerticalScrollBar().setUnitIncrement(50);
+  }
 
-	public void setZoomLevel(double zoomLevel) {
-		horizontalRuler.setZoomLevel(zoomLevel);
-		verticalRuler.setZoomLevel(zoomLevel);
-	}
+  public void setRulerVisible(boolean visible) {
+    if (visible) {
+      setColumnHeaderView(horizontalRuler);
+      setRowHeaderView(verticalRuler);
+      setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, unitButton);
+      setCorner(ScrollPaneConstants.UPPER_RIGHT_CORNER, topRightCorner);
+      setCorner(ScrollPaneConstants.LOWER_LEFT_CORNER, bottomLeftCorner);
+    } else {
+      setColumnHeaderView(null);
+      setRowHeaderView(null);
+      setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, null);
+      setCorner(ScrollPaneConstants.UPPER_RIGHT_CORNER, null);
+      setCorner(ScrollPaneConstants.LOWER_LEFT_CORNER, null);
+    }
+  }
 
-	public void setMetric(boolean isMetric) {
-		if (isMetric) {
-			horizontalRuler.setIsMetric(true);
-			verticalRuler.setIsMetric(true);
-			unitButton.setText("cm");
-		} else {
-			horizontalRuler.setIsMetric(false);
-			verticalRuler.setIsMetric(false);
-			unitButton.setText("in");
-		}
-		for (IRulerListener listener : listeners) {
-			listener.unitsChanged(isMetric);
-		}
-	}
+  public void setSelectionRectangle(Rectangle2D rect) {
+    horizontalRuler.setSelectionRect(rect);
+    verticalRuler.setSelectionRect(rect);
+  }
 
-	protected void updateRulerSize(int width, int height) {
-		horizontalRuler.setPreferredWidth(width);
-		horizontalRuler.invalidate();
-		verticalRuler.setPreferredHeight(height);
-		verticalRuler.invalidate();
-	}
+  public boolean addUnitListener(IRulerListener e) {
+    return listeners.add(e);
+  }
 
-	class Corner extends JComponent {
+  public boolean removeUnitListener(IRulerListener o) {
+    return listeners.remove(o);
+  }
 
-		private static final long serialVersionUID = 1L;
-		private final int orientation;
+  public void setZoomLevel(double zoomLevel) {
+    horizontalRuler.setZoomLevel(zoomLevel);
+    verticalRuler.setZoomLevel(zoomLevel);
+  }
 
-		public Corner(int orientation) {
-			this.orientation = orientation;
-		}
+  public void setMetric(boolean isMetric) {
+    if (isMetric) {
+      horizontalRuler.setIsMetric(true);
+      verticalRuler.setIsMetric(true);
+      unitButton.setText("cm");
+    } else {
+      horizontalRuler.setIsMetric(false);
+      verticalRuler.setIsMetric(false);
+      unitButton.setText("in");
+    }
+    for (IRulerListener listener : listeners) {
+      listener.unitsChanged(isMetric);
+    }
+  }
 
-		protected void paintComponent(Graphics g) {
-			g.setColor(Ruler.COLOR);
-			g.fillRect(0, 0, getWidth(), getHeight());
-			g.setColor(Color.black);
-			if (orientation == Ruler.HORIZONTAL) {
-				g.drawLine(0, getHeight() - 1, getWidth() - 1, getHeight() - 1);
-			} else if (orientation == Ruler.VERTICAL) {
-				g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight() - 1);
-			}
-		}
-	}
+  protected void updateRulerSize(int width, int height) {
+    horizontalRuler.setPreferredWidth(width);
+    horizontalRuler.invalidate();
+    verticalRuler.setPreferredHeight(height);
+    verticalRuler.invalidate();
+  }
+
+  class Corner extends JComponent {
+
+    private static final long serialVersionUID = 1L;
+    private final int orientation;
+
+    public Corner(int orientation) {
+      this.orientation = orientation;
+    }
+
+    protected void paintComponent(Graphics g) {
+      g.setColor(Ruler.COLOR);
+      g.fillRect(0, 0, getWidth(), getHeight());
+      g.setColor(Color.black);
+      if (orientation == Ruler.HORIZONTAL) {
+        g.drawLine(0, getHeight() - 1, getWidth() - 1, getHeight() - 1);
+      } else if (orientation == Ruler.VERTICAL) {
+        g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight() - 1);
+      }
+    }
+  }
 }
